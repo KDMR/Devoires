28025fa58a18bebaa61d716aa7561d77b377bf0b
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-11-15 17:26:28 +0330

    Editing example files 1 and 2

--- python/example_2_some_string_methods.py
+++ python/example_2_some_string_methods.py
@@ -1,7 +1,23 @@
 # creating an string
 verse = "abara kadabra"
 
+
 # printing some string edits
 print(verse.title())
 print(verse.upper())
 print(verse.lower())
+print("\n\n")
+
+# initialize a string variable 
+# and then printing the third character of it 
+# at index no. `2`
+myString = "lorem ipsum, dolor sit amet"
+print (myString[2])	# prints r
+print("\n\n")
+
+# stripping whitespaces from a string variable's sorrowndings
+name = " Ada "
+print (name.strip())	# prints `Ada`
+print (name.rstrip())	# prints ` Ada`
+print (name.lstrip())	# prints `Ada `
+print("\n\n")
