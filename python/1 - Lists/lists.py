e8f6a359f147d825df1628235c4eeee9ba06ee95
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-06 03:28:36 +0330

    Initial commit for the lists examples

---
+++ python/1 - Lists/lists.py
@@ -0,0 +1,28 @@
+
+# Defining some lists
+names_human = ["Koorosh", "Morgane", "Justin", "Megan", "Till", "Marianna", "Borin", "Katherine", "Lee", "Anastasia"]
+names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot", "Citroen", "Alpine"]
+names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202", "Simon", "Micral N"]
+
+# Printing the names of humans
+print("\n")
+print("Some human names are printed below :")
+print("------------------------------------")
+for name in names_human:
+	print(name)
+
+
+# Printing the names of some early time computers
+print("\n")
+print("Some old time's computer names are printed below :")
+print("------------------------------------------------")
+for name in names_computers:
+	print(name)
+
+
+# Printing the names of cars
+print("\n")
+print("Some recent car names are printed below :")
+print("-----------------------------------------")
+for name in names_cars:
+	print(name)
