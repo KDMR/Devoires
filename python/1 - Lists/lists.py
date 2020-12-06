00c5bf0516fdb31aae4ddb0c3ab3c02e934686ba
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-06 04:22:45 +0330

    Added the pop and remove methods for taking out values from a list

--- python/1 - Lists/lists.py
+++ python/1 - Lists/lists.py
@@ -1,28 +1,85 @@
 
 # Defining some lists
-names_human = ["Koorosh", "Morgane", "Justin", "Megan", "Till", "Marianna", "Borin", "Katherine", "Lee", "Anastasia"]
+names_human = ["Koorosh", "Morgane", "Pauline", "Justin", "Megan", "Till", "Marianna", "Borin", "Katherine", "Lee", "Anastasia", "Shyam", "Salona"]
 names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot", "Citroen", "Alpine"]
 names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202", "Simon", "Micral N"]
 
+
+
 # Printing the names of humans
-print("\n")
-print("Some human names are printed below :")
-print("------------------------------------")
-for name in names_human:
-	print(name)
+# print("\n")
+# print("Some human names are printed below :")
+# print("------------------------------------")
+# for name in names_human:
+# 	print(name)
+
 
 
 # Printing the names of some early time computers
-print("\n")
-print("Some old time's computer names are printed below :")
-print("------------------------------------------------")
-for name in names_computers:
-	print(name)
+# print("\n")
+# print("Some old time's computer names are printed below :")
+# print("------------------------------------------------")
+# for name in names_computers:
+# 	print(name)
+
 
 
 # Printing the names of cars
-print("\n")
-print("Some recent car names are printed below :")
-print("-----------------------------------------")
-for name in names_cars:
-	print(name)
+# print("\n")
+# print("Some recent car names are printed below :")
+# print("-----------------------------------------")
+# for name in names_cars:
+# 	print(name)
+
+
+
+# Insert into the end (right side) of the lists in Python
+# print("\n")
+# names_human.append("Issau")
+# print("The new list of human names is:")
+# print(names_human)
+
+
+
+# Insert into the beginning (left side) of the lists in Python
+# print("\n")
+# names_human.insert(0, "Konrad")
+# print("The new list of human names is:")
+# print(names_human)
+
+
+
+# Insert a name into the index 3 (fourth element of the list)
+# print("\n")
+# names_human.insert(3, "Courage")
+# print("The new list of human names is:")
+# print(names_human)
+
+
+
+# Removing an item from the end of list using the pop() method
+# popped_name = names_human.pop()
+# print("Last item of the list (popped one) is:")
+# print(popped_name)
+# print("The list is now like:")
+# print(names_human)
+
+
+
+# Removing an item by it's index number
+# popped_name = names_human.pop(0)
+# print("Last item of the list (popped one) is:")
+# print(popped_name)
+# print("The list is now like:")
+# print(names_human)
+
+
+
+# Removing an item by it's value
+# names_human.remove("Koorosh")
+# print("The list is now like:")
+# print(names_human)
+# print("\n")
+
+
+
