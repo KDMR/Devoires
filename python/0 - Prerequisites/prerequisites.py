c2f02f561ed994d6a45209aba6c311e02de1c12f
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-07 02:39:33 +0330

    Added Prerequisites folder and file: prerequisites.py inside it.

---
+++ python/0 - Prerequisites/prerequisites.py
@@ -0,0 +1,10 @@
+
+# How to print a sentence
+print("Hello, World!")
+
+
+
+# How to print a string that contains some variables
+f_name = "Koorosh"
+l_name = "Sadeghi Tehran"
+print("Welcome to the world of Python, {0} {1}.".format(f_name, l_name))
