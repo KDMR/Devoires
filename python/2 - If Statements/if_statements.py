dca80180f13ebc71dd87fa710a689984abf9737d
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-07 09:40:02 +0330

    Provided enough basic information on how to work with if statement in python.

--- python/2 - If Statements/if_statements.py
+++ python/2 - If Statements/if_statements.py
@@ -2,6 +2,7 @@
 names_human = ["Koorosh", "Morgane", "Pauline", "Justin", "Megan", "Till", "Marianna", "Borin", "Katherine", "Lee", "Anastasia", "Shyam", "Salona"]
 names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot", "Citroen", "Alpine"]
 names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202", "Simon", "Micral N"]
+names_animal = []
 
 
 
@@ -12,6 +13,43 @@ names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202"
 # 	print("{0} is in the list: {1}".format(name, list_name))
 # else:
 # 	print("{0} is not in the list: {1}".format(name, list_name))
+
+
+
+### How to check for variable inequality
+# name = "Robin"
+# if name != "Robin":
+# 	print("Name is not {0}".format(name))
+# else:
+# 	print("Name is {0}".format(name))
+
+
+
+### Checking for multiple conditions
+# name = "Marie"
+# car = "BMW"
+# if name in names_human and car in names_cars:
+# 	print("{0}'s car is of type: {1}.".format(name, car))
+# else:
+# 	print("{0}'s car is not of type: {1}.".format(name, car))
+
+
+
+### Checking if a value does not exist in a list
+# name = "Mike"
+# if name not in names_human:
+# 	print("{0} is not included in our list.".format(name))
+
+
+
+### Checking to know if a list is empty or not
+### If the list is not empty it returns True
+# list1_name = "names_cars"
+# list2_name = "names_animal"
+# if names_cars and names_animal:
+# 	print("Both {0} and {1} lists have at least one member.".format("names_cars", "names_animal"))
+# else:
+# 	print("At least one of the lists {0} or {1} is empty.".format("names_cars", "names_animal"))
 
 
 
