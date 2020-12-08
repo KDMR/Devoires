7d4d9c0eaf9074b0dbe40fc0f6a775793de8106d
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-08 03:33:52 +0330

    How to create dictionaries

---
+++ python/3 - Dictionaries/dictionaries.py
@@ -0,0 +1,18 @@
+#/////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////  Dictionaries   ///////////////////////////////////
+#/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+### On how a dictionaty is
+# user1_info = {"fname":"Grace", "lname":"Hopper"}
+# user2_info = {"fname":"mike", "lname":"Muuss"}
+# user3_info = {"fname":"Dennis", "lname":"Ritchie"}
+# user4_info = {"fname":"Ken", "lname":"Thompson"}
+# print("User_1's first name is: " + user1_info["fname"])
+# print("User_1's last name is: " + user1_info["lname"])
+# print("User_1's first name is: " + user2_info["fname"])
+# print("User_1's last name is: " + user2_info["lname"])
+
+
+
