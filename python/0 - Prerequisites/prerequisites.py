0696a5fe01d9f46e0cb14f05d4a895ab9d548b6d
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-09 02:22:21 +0330

    Adding dir and help functions' descriptions and doing a little bit of content refinements and re-arrangements

--- python/0 - Prerequisites/prerequisites.py
+++ python/0 - Prerequisites/prerequisites.py
@@ -1,10 +1,42 @@
+#!/usr/bin/python3
+### How to find the methods and properties
+### of a variable, object, ...
+# class Person:
+#   name = "Loulou"
+#   age = 42
+#   country = "France"
+# print(dir(Person))
 
-# How to print a sentence
-print("Hello, World!")
+
+
+### How to view the documentation of elements?
+### The python help function is used to display
+### the documentation of modules, functions,
+### classes, keywords etc (https://www.geeksforgeeks.org/help-function-in-python/)
+# class Person:
+#   name = "Loulou"
+#   age = 42
+#   country = "France"
+# help(Person)
 
 
 
-# How to print a string that contains some variables
-f_name = "Koorosh"
-l_name = "Sadeghi Tehran"
-print("Welcome to the world of Python, {0} {1}.".format(f_name, l_name))
+### How to know what a variable's type is?
+# class Person:
+#   name = "Loulou"
+#   age = 42
+#   country = "France"
+# var = dir(Person)
+# print(type(var))  # Prints <type 'list'>
+
+
+
+### How to print a sentence
+# print("Hello, World!")
+
+
+
+### How to print a string that contains some variables
+# f_name = "Koorosh"
+# l_name = "Sadeghi Tehran"
+# print("Welcome to the world of Python, {0} {1}.".format(f_name, l_name))
