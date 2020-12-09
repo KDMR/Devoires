d6e835ce51efd127ec7ca1a3a697a3a8259fd94a
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-10 02:32:14 +0330

    Learning about the return statement

--- python/5 - Functions/functions.py
+++ python/5 - Functions/functions.py
@@ -4,6 +4,7 @@
 #/////////////////////////////////////////////////////////////////////////////////////////
 
 names_human = ["Koorosh", "Morgane", "Pauline", "Justin", "Megan", "Till", "Marianna", "Borin", "Katherine", "Lee", "Anastasia", "Shyam", "Salona", "Estragon", "Vladimir", "Pozzo", "Godot", "Lucky", "Gary"]
+names_artists = {"janis joplin": "singer", "william turner": "painter", "pier paolo pasolini": "film director"}
 names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot", "Citroen", "Alpine"]
 names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202", "Simon", "Micral N"]
 user1_info = {"fname":"Grace", "lname":"Hopper"}
@@ -55,6 +56,16 @@ car = {"Window": 4, "Wheel": 4, "Antenna": 1, "Wheel": 40}
 # animal_care("Puppy", "Grooming")
 # animal_care("caty")
 # animal_care("Piggy", "Washing")
+
+
+
+### How does retutn value work in python?
+# def artist(name, style):
+# 	artist_description = name.title() + " was a " + style.title() + "."
+# 	return artist_description
+# for name, style in names_artists.items():
+# 	result = artist(name, style)
+# 	print(result)
 
 
 
