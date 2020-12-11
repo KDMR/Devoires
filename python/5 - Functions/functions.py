53c2bdd0ba4ecba31bf0efc13c45e621b26ec45c
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-11 05:06:01 +0330

    Made some subtle changes on how the text should look

--- python/5 - Functions/functions.py
+++ python/5 - Functions/functions.py
@@ -138,7 +138,7 @@ car = {"Window": 4, "Wheel": 4, "Antenna": 1, "Wheel": 40}
 
 
 
-### How to use a function that is written in a module 
+### How to use a function that is written in amodule 
 # WHEN IMPORTING, CONSIDER THE FACT THAT IMPORTING THE ENTIRE MODULE IS DONE BY IMPORTING
 # ITS FILENAME. THIS MODULE MIGHT CONTAIN A METHOD AS THE NAME AS THE MODULE ITSELF. 
 # AS A RESULT, DO NOT FORGET TO USE THE METHOD AS THE FOLLOWING WAYS:
@@ -146,8 +146,10 @@ car = {"Window": 4, "Wheel": 4, "Antenna": 1, "Wheel": 40}
 # 		Example:
 # 		import pprint
 # 		pprint.pprint(myDict)
-
 # 2)	If importing the method from module:
 # 		Example:
 # 		from pprint import pprint
 # 		pprint(myDict)
+
+
+
