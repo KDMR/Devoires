9785470f9558465523ce21b6b869c8e4bc2a0973
Koorosh Sadeghi Tehran <koorosh13hm@gmail.com>
2020-12-19 12:00:24 +0330

    Starting to learn Python OOP

--- testing-area/python/index.py
+++ testing-area/python/index.py
@@ -3,34 +3,35 @@
 #/////////////////////////////////////  Testing-Area   ///////////////////////////////////
 #/////////////////////////////////////////////////////////////////////////////////////////
 
-names_human = ["Koorosh", "Morgane", "Pauline", "Justin", "Megan", "Till",
-							 "Marianna", "Borin", "Katherine", "Lee", "Anastasia", "Shyam",
-							 "Salona", "Estragon", "Vladimir", "Pozzo", "Godot", "Lucky", "Gary"]
-names_artists = {"janis joplin": "singer", "william turner": "painter",
-								 "pier paolo pasolini": "film director"}
-names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot",
-							"Citroen", "Alpine"]
-names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202",
-									 "Simon", "Micral N"]
-user1_info = {"fname":"Grace", "lname":"Hopper", "address": "",
-							"children": , "company": "navy"}
-user2_info = {"fname":"mike", "lname":"Muuss", "address": "",
-							"children": , "company": "darpa"}
-user3_info = {"fname":"Dennis", "lname":"Ritchie", "address": "",
-							"children": , "company": "bell labs"}
-user4_info = {"fname":"Ken", "lname":"Thompson", "address": "",
-							"children": , "company": "bell labs"}
-car = {"Window": 4, "Wheel": 4, "Antenna": 1, "Wheel": 40}
+# names_human = ["Koorosh", "Morgane", "Pauline", "Justin", "Megan", "Till",
+# 							 "Marianna", "Borin", "Katherine", "Lee", "Anastasia", "Shyam",
+# 							 "Salona", "Estragon", "Vladimir", "Pozzo", "Godot", "Lucky", "Gary"]
+# names_artists = {"janis joplin": "singer", "william turner": "painter",
+# 								 "pier paolo pasolini": "film director"}
+# names_cars = ["Bugatti", "BMW", "Peykan", "Renault", "Mercedes-Benz", "Peugeot",
+# 							"Citroen", "Alpine"]
+# names_computers = ["Altaire 8800", "IBM 610", "Kenbak-1", "MIR", "Datapoint 202",
+# 									 "Simon", "Micral N"]
+# user1_info = {"fname":"Grace", "lname":"Hopper", "address": "Nan","children": 0, "company": "navy"}
+# user2_info = {"fname":"mike", "lname":"Muuss", "address": "",
+# 							"children": , "company": "darpa"}
+# user3_info = {"fname":"Dennis", "lname":"Ritchie", "address": "",
+# 							"children": , "company": "bell labs"}
+# user4_info = {"fname":"Ken", "lname":"Thompson", "address": "",
+# 							"children": , "company": "bell labs"}
+# car = {"Window": 4, "Wheel": 4, "Antenna": 1, "Wheel": 40}
 
 ###########################################################################################
 ###########################################################################################
 ###########################################################################################
 
 
+class buy:
+	def __init__(self):
+		print("Hello from the init function!")
 
 
-
-
+testfunc = buy()
 
 
 
